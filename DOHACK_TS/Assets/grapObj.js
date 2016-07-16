a81d54172235cc3bd8a1fe943682c8ec7731939a
Kazutos <gibiberg@web.de>
2016-07-17 00:05:16 +0200

    Unity Test

---
+++ DOHACK_TS/Assets/grapObj.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var pos0: Vector3;
+var speed: float = 5f;
+
+function Start () {
+	pos0 = transform.position;
+}
+
+function Update () {
+	transform.Translate(Vector3(-1, 0, 0) * speed * Time.deltaTime);
+	if(transform.position.x < -pos0.x){
+		transform.position = pos0;
+	}
+}
